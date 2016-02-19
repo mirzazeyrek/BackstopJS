26d0239775aac7d492007db7c9865f3475397b38
Uğur Mirza Zeyrek <mirza@artbees.net>
2016-02-19 17:19:41 +0200

    memory error fix when having more than 500 images.

--- gulp/tasks/compare.js
+++ gulp/tasks/compare.js
@@ -8,7 +8,7 @@ var _ = require('underscore');
 
 gulp.task('compare', function (done) {
   var compareConfig = JSON.parse(fs.readFileSync(paths.compareConfigFileName, 'utf8')).compareConfig;
-
+  var simultanious_comparsion = 0;
   function updateProgress() {
     var results = {};
     _.each(compareConfig.testPairs, function (pair) {
@@ -37,25 +37,57 @@ gulp.task('compare', function (done) {
 
 
   _.each(compareConfig.testPairs, function (pair) {
+    simultanious_comparsion++;
     pair.testStatus = "running";
-
     var referencePath = path.join(paths.backstop, pair.reference);
     var testPath = path.join(paths.backstop, pair.test);
+    if(simultanious_comparsion>100) {
+      console.log("waiting in the line");
+      console.log(simultanious_comparsion);
+      console.log(testPath);
+    } else {
+      console.log(simultanious_comparsion);
+      resemble(referencePath).compareTo(testPath).onComplete(function (data) {
+        var imageComparisonFailed = !data.isSameDimensions || data.misMatchPercentage > pair.misMatchThreshold;
 
+        if (imageComparisonFailed) {
+          pair.testStatus = "fail";
+          simultanious_comparsion--;
+          console.log('ERROR:', pair.label, pair.fileName);
+          storeFailedDiffImage(testPath, data);
+          startComparsion(testPath, referencePath);
+        } else {
+          pair.testStatus = "pass";
+          simultanious_comparsion--;
+          console.log('OK:', pair.label, pair.fileName);
+          startComparsion(testPath, referencePath);
+        }
+
+        updateProgress();
+      });
+    }
+  });
+
+  function startComparsion(testPath, referencePath) {
     resemble(referencePath).compareTo(testPath).onComplete(function (data) {
       var imageComparisonFailed = !data.isSameDimensions || data.misMatchPercentage > pair.misMatchThreshold;
 
       if (imageComparisonFailed) {
         pair.testStatus = "fail";
+        simultanious_comparsion--;
         console.log('ERROR:', pair.label, pair.fileName);
         storeFailedDiffImage(testPath, data);
+
       } else {
         pair.testStatus = "pass";
+        simultanious_comparsion--;
         console.log('OK:', pair.label, pair.fileName);
+
       }
+
       updateProgress();
     });
-  });
+  }
 
   function storeFailedDiffImage(testPath, data) {
     var failedDiffFilename = getFailedDiffFilename(testPath);
